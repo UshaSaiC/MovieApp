39426f1fac6c11dca8b4d76de23670c87792c90a
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-26 19:08:58 +0530

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Usha Sai Chintha on 26/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
