a6918c4f1393ee59d93b9617a45e4a142fe40667
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-10-08 21:44:04 +0530

    Updated tests

--- MovieTests/MovieViewModelTests.swift
+++ MovieTests/MovieViewModelTests.swift
@@ -12,33 +12,111 @@ class MovieViewModelTests: XCTestCase{
     func testSuccessFetchData() {
         let expectedResult = MovieData.with()
         let service = MockService(mockData: expectedResult)
-
+        
         let viewModel = MovieViewModel(service: service)
-
-            viewModel.getData()
-            XCTAssertEqual(viewModel.cards.count, 0)
+        
+        viewModel.getData()
+        let expectation = self.expectation(description: "Test")
+        DispatchQueue.main.async {
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 1, handler: nil)
+        XCTAssertEqual(viewModel.cards.count, 1)
+        XCTAssertEqual(viewModel.cards.first?.content.title, "movie title")
+        XCTAssertEqual(viewModel.cards.first?.content.description, "description of movie")
+        XCTAssertEqual(viewModel.cards.first?.content.movieLogo, "image of movie logo")
+        XCTAssertEqual(viewModel.cards.first?.content.rating, 6.798)
     }
     
     func testEmptyData() {
         let expectedResult = MovieData(data: Cards(cards: []))
         let service = MockService(mockData: expectedResult)
-
+        
         let viewModel = MovieViewModel(service: service)
-
+        
         viewModel.getData()
-
+        
+        let expectation = self.expectation(description: "Test")
+        DispatchQueue.main.async {
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 1, handler: nil)
         XCTAssertEqual(viewModel.cards.count, 0)
     }
-
+    
     func testNilData() {
         let expectedResult: MovieData? = nil
         let service = MockService(mockData: expectedResult)
-
+        
         let viewModel = MovieViewModel(service: service)
-
+        
         viewModel.getData()
         
+        let expectation = self.expectation(description: "Test")
+        DispatchQueue.main.async {
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 1, handler: nil)
         XCTAssertEqual(viewModel.cards.count, 0)
+    }
+    
+    func testSearchResultsWhenEmpty() {
+        let expectedResult = MovieData.with()
+        let service = MockService(mockData: expectedResult)
+        
+        let viewModel = MovieViewModel(service: service)
+        
+        viewModel.searchText = ""
+        viewModel.getData()
+        let expectation = self.expectation(description: "Test")
+        DispatchQueue.main.async {
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 1, handler: nil)
+        
+        viewModel.searchResults()
+        XCTAssertEqual(viewModel.cards.count, 1)
+        XCTAssertEqual(viewModel.cards.first?.content.title, "movie title")
+        XCTAssertEqual(viewModel.cards.first?.content.description, "description of movie")
+        XCTAssertEqual(viewModel.cards.first?.content.movieLogo, "image of movie logo")
+        XCTAssertEqual(viewModel.cards.first?.content.rating, 6.798)
+    }
+    
+    func testSearchResultsForInvalidSearchValue() {
+        let expectedResult = MovieData.with()
+        let service = MockService(mockData: expectedResult)
+        
+        let viewModel = MovieViewModel(service: service)
+        
+        viewModel.searchText = "invalid"
+        viewModel.getData()
+        let expectation = self.expectation(description: "Test")
+        DispatchQueue.main.async {
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 1, handler: nil)
+        
+        viewModel.searchResults()
+        XCTAssertEqual(viewModel.cards.count, 0)
+    }
+    
+    func testSearchResultsForValidSearchValue() {
+        let expectedResult = MovieData.with()
+        let service = MockService(mockData: expectedResult)
+        
+        let viewModel = MovieViewModel(service: service)
+        
+        viewModel.searchText = "title"
+        viewModel.getData()
+        let expectation = self.expectation(description: "Test")
+        DispatchQueue.main.async {
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 1, handler: nil)
+        
+        viewModel.searchResults()
+        XCTAssertEqual(viewModel.cards.count, 1)
+        XCTAssertEqual(viewModel.cards.first?.content.title, "movie title")
     }
 }
 
